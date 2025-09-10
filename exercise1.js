0a807d9a18b333a26990a4d8306e3ef776e2d795
dondou21 <dondouabiyi@gmail.com>
2025-09-10 11:30:49 +0200

    exercise 1 & 2 done

---
+++ exercise1.js
@@ -0,0 +1,18 @@
+
+/**
+ * function calculateAverage(numbers) {
+  var sum = 0;
+  var count = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+    count++;
+  }
+  var average = sum / count;
+  return average;
+}
+ */
+
+function calculateAverage(numbers) {
+    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue,0)/numbers.length
+}
+
